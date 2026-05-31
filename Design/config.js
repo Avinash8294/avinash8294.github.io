e87a9f750eeff4aecf23daada686d3a5c61a3257
Avinash8294 <avi82bgp@gmail.com>
2026-05-31 21:16:11 +0530

    updated forms and form data

---
+++ Design/config.js
@@ -0,0 +1,5 @@
+// Set the API base URL for form submissions
+// Uncomment ONLY ONE line according to your environment
+
+window.BASE_API_URL = "https://orangered-marten-118101.hostingersite.com/forms/"; // Hostinger production
+// window.BASE_API_URL = "http://localhost:3000/forms/"; // Local development
